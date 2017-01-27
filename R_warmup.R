bd807a46c0ebf42a78c927241e45946a1ba681d5
henryf6 <henry.frye@uconn.edu>
2017-01-27 16:47:46 -0500

    getting files setup

---
+++ R_warmup.R
@@ -0,0 +1,27 @@
+##################################
+## R warmup answers/exerices
+## Author: Henry Frye
+##################################
+
+####Objects and Arithmetic####
+#Exercise 1
+x<- c(5,9,2,3,4,6,7,0,8,12,2,9) 
+
+#a
+x[2]
+# a is 9
+
+#b 
+x[2:4]
+# 9,2,3
+
+#c
+x[c(2,3,6)]
+#9,2, 6
+
+#Exercise 2
+
+y<-c(33,44,29,16,25,45,33,19,54,22,21,49,11,24,56)
+summary(y)
+
+####Summaries and Subscripting #####
